fb5de2e26c853352a8f56345b42ddcc68517d5f3
Luigi Pinca <luigipinca@gmail.com>
2015-10-21 18:12:37 +0200

    Build the `changeName` action

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import jsondiffpatch from 'jsondiffpatch';
+import { diff } from 'jsondiffpatch';
 
 /**
  * Calculates the differences between two customer objects and builds the update
@@ -12,7 +12,49 @@ import jsondiffpatch from 'jsondiffpatch';
  * @return {Array} Update actions
  */
 function buildActions(before, after) {
+  const changes = diff(before, after) || {};
+  const nameFields = [
+    'middleName',
+    'firstName',
+    'lastName',
+    'title'
+  ];
+  const actions = [];
 
+  Object.keys(changes).forEach((key) => {
+    const change = changes[key];
+
+    if (~nameFields.indexOf(key)) {
+      //
+      // Here we assume that the new object is validated, so if there is a
+      // difference in the `firstName` or `lastName` field, this difference
+      // must consist in a different non empty string.
+      //
+      let action = actions.find((action) => {
+        return action.action === 'changeName' ? true : false;
+      });
+
+      //
+      // Create and append the action if we can't find one.
+      //
+      if (!action) {
+        action = {
+          firstName: before.firstName,
+          lastName: before.lastName,
+          action: 'changeName'
+        };
+        actions.push(action);
+      }
+
+      if (change.length === 1) {
+        action[key] = change[0];
+      } else if (change.length === 2) {
+        action[key] = change[1];
+      }
+    }
+  });
+
+  return actions;
 }
 
 //
--- test/test.js
+++ test/test.js
@@ -1,10 +1,32 @@
 'use strict';
 
+import customer from './fixture/customer';
 import buildActions from '../src/index';
+import clone from 'lodash.clonedeep';
 import { expect } from 'chai';
 
 describe('buildActions', () => {
   it('is exported as a function', () => {
     expect(buildActions).to.be.a('function');
+  });
+
+  it('returns an empty array if there are no changes', () => {
+    expect(buildActions(customer, customer)).to.deep.equal([]);
+  });
+
+  it('builds the `changeName` action', () => {
+    const before = clone(customer);
+    const after = clone(customer);
+
+    after.middleName = 'qux';
+    after.firstName = 'baz';
+    before.title = 'Mr.';
+
+    expect(buildActions(before, after)).to.deep.equal([{
+      action: 'changeName',
+      middleName: after.middleName,
+      firstName: after.firstName,
+      lastName: 'bar'
+    }]);
   });
 });
